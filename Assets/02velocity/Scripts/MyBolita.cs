7395aaa20bc8198d2b67f8e792e8b640f47073c0
Viktortilla <87790676+Viktortilla@users.noreply.github.com>
2022-08-30 14:05:32 -0500

    clase 8
    acceleracion y cambio de direccion

--- Assets/02velocity/Scripts/MyBolita.cs
+++ Assets/02velocity/Scripts/MyBolita.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class MyBolita : MonoBehaviour
 {
     private MyVector2D position;
-    [SerializeField] private MyVector2D displacement;
+    [SerializeField] private MyVector2D acceleration;
     [SerializeField] private MyVector2D velocity;
+    [Range(0f,1f)][SerializeField] private float dampingFactor=0.9f;
     [Header("World")]
     [SerializeField] Camera camera;
+    private byte cont = 0;
+    private float x;
+    private float y;
 
     void Start()
     {
@@ -19,25 +23,53 @@ public class MyBolita : MonoBehaviour
 
     void Update()
     {
+        velocity = velocity + acceleration * Time.deltaTime;
         position = new MyVector2D(transform.position.x, transform.position.y);
         velocity.Draw(position,Color.red);
         position.Draw(Color.blue);
+        acceleration.Draw(Color.green);
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))//si se presiona espacio
+        {
+            cont++;
+            if (cont == 1)
+            {
+                x = -acceleration.y;
+                y = -acceleration.x;
+                acceleration = new MyVector2D(x, y);
+                velocity = new MyVector2D(0,0);
+            }
+            if (cont == 2)
+            {
+                x = acceleration.y;
+                y = acceleration.x;
+                acceleration = new MyVector2D(x, y);
+                cont = 0;
+                velocity = new MyVector2D(0, 0);
+
+            }
+        }
+
+
     }
     public void Move()
     {
-        displacement = velocity * Time.deltaTime;
+        
         position = position + velocity * Time.deltaTime;
 
         if(Mathf.Abs(position.x)> camera.orthographicSize)
         {
             velocity.x = velocity.x * (-1);
             position.x = Mathf.Sign(position.x) * camera.orthographicSize;
+            velocity *= dampingFactor;
+
         }
         if (Mathf.Abs(position.y) > camera.orthographicSize)
         {
             velocity.y = velocity.y * (-1);
             position.y = Mathf.Sign(position.y) * camera.orthographicSize;
+            velocity *= dampingFactor;
         }
 
         transform.position = new Vector3(position.x,position.y);
